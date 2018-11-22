5dc9ca305dfd63654e6b08e46d340fc684a3e7dc
serzhikkk <sergey-andreev-97-97@mail.ru>
2018-11-22 10:46:39 +0300

    Added recompose to TodoAddForm

--- src/containers/forms/TodoAddForm/index.js
+++ src/containers/forms/TodoAddForm/index.js
@@ -1,44 +1,44 @@
 import React from 'react';
+import {
+  compose,
+  withState,
+  pure,
+  withHandlers,
+} from 'recompose';
 
 import './styles.css';
 
-export default class TodoAddFrom extends React.Component {
-  state = {
-    label: '',
-  };
+const TodoAddForm = ({
+  label,
+  setLabel,
+  onSubmit,
+}) => (
+  <form onSubmit={onSubmit} className="item-add-form input-group d-flex">
+    <input
+      className="form-control"
+      type="text"
+      onChange={e => setLabel(e.target.value)}
+      placeholder="What needs to be done?"
+      value={label}
+    />
+    <button type="submit" className="btn btn-outline-secondary ml-1">
+      Add Todo
+    </button>
+  </form>
+);
 
-  onLabelChange = (event) => {
-    this.setState({
-      label: event.target.value,
-    });
-  }
-
-  onSubmit = (event) => {
-    event.preventDefault();
-    const { onCreateTodo } = this.props;
-    const { label } = this.state;
-    onCreateTodo(label);
-    this.setState({
-      label: '',
-    });
-  }
-
-  render() {
-    const { label } = this.state;
-
-    return (
-      <form onSubmit={this.onSubmit} className="item-add-form input-group d-flex">
-        <input
-          className="form-control"
-          type="text"
-          onChange={this.onLabelChange}
-          placeholder="What needs to be done?"
-          value={label}
-        />
-        <button type="submit" className="btn btn-outline-secondary ml-1">
-          Add Todo
-        </button>
-      </form>
-    );
-  }
-}
+export default compose(
+  pure,
+  withState('label', 'setLabel', ''),
+  withHandlers({
+    onSubmit: ({
+      onCreateTodo,
+      label,
+      setLabel,
+    }) => (event) => {
+      event.preventDefault();
+      onCreateTodo(label);
+      setLabel('');
+    },
+  }),
+)(TodoAddForm);
